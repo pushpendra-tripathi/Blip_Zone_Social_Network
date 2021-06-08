bab60a30498d84e78558697a726aa688caa28517
Pushpendra Tripathi <pushpendratripathi@outlook.com>
2021-06-08 19:00:21 +0530

    comments are inflated in layout

--- app/src/main/java/com/starlord/blipzone/adapters/CommentsAdapter.java
+++ app/src/main/java/com/starlord/blipzone/adapters/CommentsAdapter.java
@@ -1,17 +1,23 @@
 package com.starlord.blipzone.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+import com.starlord.blipzone.R;
 import com.starlord.blipzone.models.CommentModel;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.CommentsViewHolder> {
     Context context;
@@ -26,22 +32,37 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Commen
     @NotNull
     @Override
     public CommentsViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_comments, null);
+        return new CommentsViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull CommentsAdapter.CommentsViewHolder holder, int position) {
+        CommentModel commentModel = commentModelArrayList.get(position);
+        Picasso.get().load(commentModel.getUserModel().getProfileImage())
+                .placeholder(R.drawable.profile_avatar)
+                .into(holder.profileImage);
+
+        holder.commentUserName.setText(commentModel.getUserModel().getUserName());
+        holder.comment.setText(commentModel.getContent());
+        holder.commentTime.setText(commentModel.getCreatedAt());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return commentModelArrayList.size();
     }
 
     public class CommentsViewHolder extends RecyclerView.ViewHolder {
+        CircleImageView profileImage;
+        TextView commentUserName, comment, commentTime;
         public CommentsViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
+            profileImage = itemView.findViewById(R.id.comment_profile_image);
+            commentUserName = itemView.findViewById(R.id.comment_username);
+            comment = itemView.findViewById(R.id.comment);
+            commentTime = itemView.findViewById(R.id.comment_time_posted);
         }
     }
 }
--- app/src/main/java/com/starlord/blipzone/views/CommentsActivity.java
+++ app/src/main/java/com/starlord/blipzone/views/CommentsActivity.java
@@ -1,16 +1,124 @@
 package com.starlord.blipzone.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.VolleyError;
 import com.starlord.blipzone.R;
+import com.starlord.blipzone.adapters.CommentsAdapter;
+import com.starlord.blipzone.api.CommonClassForAPI;
+import com.starlord.blipzone.callbacks.ApiResultCallback;
+import com.starlord.blipzone.configurations.UrlConstants;
+import com.starlord.blipzone.models.BlogModel;
+import com.starlord.blipzone.models.CommentModel;
+import com.starlord.blipzone.models.LikeModel;
+import com.starlord.blipzone.models.UserModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.starlord.blipzone.api.CommonClassForAPI.*;
+import static com.starlord.blipzone.configurations.UrlConstants.*;
 
 public class CommentsActivity extends AppCompatActivity {
+    String blogId;
+    ImageView backBtn, sendBtn;
+    TextView title;
+    RecyclerView commentsRecyclerView;
+    LinearLayoutManager linearLayoutManager;
+    EditText commentBox;
+    ArrayList<CommentModel> commentModelArrayList;
+    CommentsAdapter commentsAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
+
+        blogId = getIntent().getStringExtra("blogId");
+
+        initializeViews();
+
+        loadCommentsRequest();
+
+        backBtn.setOnClickListener(v -> {
+            onBackPressed();
+        });
+    }
+
+    private void loadCommentsRequest() {
+        callAuthGetRequest(CommentsActivity.this,
+                GET_COMMENT + blogId,
+                new ApiResultCallback() {
+                    @Override
+                    public void onAPIResultSuccess(JSONObject jsonObject) {
+                        processCommentsResponse(jsonObject);
+                    }
+
+                    @Override
+                    public void onAPIResultError(VolleyError volleyError) {
+
+                    }
+                });
+    }
+
+    private void processCommentsResponse(JSONObject jsonObject) {
+        try {
+            boolean status = jsonObject.getBoolean("status");
+
+            if (status) {
+                JSONArray data = jsonObject.getJSONArray("data");
+                for (int i = 0; i < data.length(); i++) {
+                    JSONObject comment = data.getJSONObject(i);
+                    CommentModel commentModel = new CommentModel();
+
+                    commentModel.setId(comment.getInt("id"));
+                    JSONObject user = comment.getJSONObject("user");
+                    UserModel userModel = new UserModel();
+                    userModel.setId(user.getInt("id"));
+                    userModel.setUserName(user.getString("username"));
+                    userModel.setFirstName(user.getString("first_name"));
+                    userModel.setLastName(user.getString("last_name"));
+                    userModel.setProfileImage(user.getString("profile_image"));
+                    commentModel.setUserModel(userModel);
+
+                    commentModel.setCreatedAt(comment.getString("created_at"));
+                    commentModel.setLastUpdatedAt(comment.getString("last_updated_at"));
+                    commentModel.setContent(comment.getString("content"));
+                    commentModel.setPostId(comment.getInt("post"));
+                    commentModelArrayList.add(commentModel);
+                    commentsAdapter.notifyDataSetChanged();
+                }
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeViews() {
+        title = findViewById(R.id.comment_title);
+        title.setText(R.string.comments);
+        backBtn = findViewById(R.id.backBtn_comment);
+        commentsRecyclerView = findViewById(R.id.comments_rv);
+        commentBox = findViewById(R.id.comment);
+        sendBtn = findViewById(R.id.ivPostComment);
+        commentModelArrayList = new ArrayList<>();
+        linearLayoutManager = new LinearLayoutManager(CommentsActivity.this);
+        commentsAdapter = new CommentsAdapter(CommentsActivity.this, commentModelArrayList);
+        commentsRecyclerView.setLayoutManager(linearLayoutManager);
+        commentsRecyclerView.setAdapter(commentsAdapter);
     }
 }

f42e3694e696eb6ccc39cd27c5682a7fdaf24938
Pushpendra Tripathi <pushpendratripathi@outlook.com>
2021-06-19 02:05:51 +0530

    fixed profile activity bug

---
+++ app/src/main/java/com/starlord/blipzone/views/activities/UserChatListActivity.java
@@ -0,0 +1,16 @@
+package com.starlord.blipzone.views.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.starlord.blipzone.R;
+
+public class UserChatListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_chat_list);
+    }
+}
